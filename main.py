567246dc57355f3f41787fd81ceac9e388b4e58a
NJN <160208242+nanajunana@users.noreply.github.com>
2025-04-01 20:02:44 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,24 +5,33 @@ screen = Screen()
 
 tim.shape('turtle')
 screen.listen()
-turtle_head = 0
+tim.speed('fastest')
 
 def forward():
     tim.forward(10)
 def backward():
     tim.backward(10)
 def left():
-    global turtle_head
-    turtle_head += 10
-    tim.setheading(turtle_head)
+    tim.left(10)
 def right():
-    global turtle_head
-    turtle_head -= 10
-    tim.setheading(turtle_head)
+    tim.right(10)
+def forward_right():
+    tim.right(10)
+    tim.forward(10)
+
+def forward_left():
+    tim.left(10)
+    tim.forward(10)
+
+def clear_drawing():
+    tim.reset()
 
 screen.onkey(key='w', fun=forward)
 screen.onkey(key='s', fun=backward)
 screen.onkey(key='d', fun=right)
 screen.onkey(key='a', fun=left)
+screen.onkey(key='e', fun=forward_right)
+screen.onkey(key='q', fun=forward_left)
+screen.onkey(key='c', fun=clear_drawing)
 
 screen.exitonclick()
